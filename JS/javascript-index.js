0e570f12892c419872076c5b2947488c531a5ea5
italorods <italo_.74@hotmail.com>
2023-07-16 17:12:22 -0300

    update geral para tamanhos de dispositivos

---
+++ JS/javascript-index.js
@@ -0,0 +1,34 @@
+    let menuIcon = document.querySelector('#menu');
+    let navbar = document.querySelector('.navbar');
+    const menuOptions = document.querySelectorAll('.navbar a');
+
+
+//toggle icon header
+    menuIcon.onclick = () => {
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    
+    }
+
+// Função para alterar o ícone para o ícone de menu e ocultar o menu
+function resetMenu() {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+  }
+  
+  // evento de click para as opções do menu
+  menuOptions.forEach(option => {
+    option.addEventListener('click', resetMenu);
+  });
+   
+//remove nav e icon quando clicar    
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+//atribuir ano no footer da index
+
+  // Obtém o ano atual
+  const year = new Date().getFullYear();
+
+  // Atualiza o conteúdo do elemento span com o ano atual
+  document.getElementById("year").textContent = year;
