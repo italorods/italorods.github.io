c1d7f3f40ed75613a9a50070bfa3d34026ef981d
italorods <italo_.74@hotmail.com>
2023-07-28 11:05:47 -0300

    alteração @media classe contato

--- JS/javascript-index.js
+++ JS/javascript-index.js
@@ -1,34 +1,44 @@
+//menu header 
+ 
     let menuIcon = document.querySelector('#menu');
     let navbar = document.querySelector('.navbar');
     const menuOptions = document.querySelectorAll('.navbar a');
 
 
-//toggle icon header
-    menuIcon.onclick = () => {
-        menuIcon.classList.toggle('bx-x');
-        navbar.classList.toggle('active');
-    
-    }
+    //toggle icon header
+        menuIcon.onclick = () => {
+            menuIcon.classList.toggle('bx-x');
+            navbar.classList.toggle('active');
+        
+        }
 
-// Função para alterar o ícone para o ícone de menu e ocultar o menu
-function resetMenu() {
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-  }
-  
-  // evento de click para as opções do menu
-  menuOptions.forEach(option => {
-    option.addEventListener('click', resetMenu);
-  });
-   
-//remove nav e icon quando clicar    
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+    // Função para alterar o ícone para o ícone de menu e ocultar o menu
+    function resetMenu() {
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
+      }
+      
+      // evento de click para as opções do menu
+      menuOptions.forEach(option => {
+        option.addEventListener('click', resetMenu);
+      });
+      
+    //remove nav e icon quando clicar    
+        menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active');
 
 //atribuir ano no footer da index
 
-  // Obtém o ano atual
-  const year = new Date().getFullYear();
+    // Obtém o ano atual
+    const year = new Date().getFullYear();
 
-  // Atualiza o conteúdo do elemento span com o ano atual
-  document.getElementById("year").textContent = year;
+    // Atualiza o conteúdo do elemento span com o ano atual
+    document.getElementById("year").textContent = year;
+
+//minha idade 
+
+    // calculo da minha idade
+    const idade = year-1998      
+
+    //atualiza o conteúdo do elemento span com minha idade atual
+    document.getElementById("idade").textContent = idade;
